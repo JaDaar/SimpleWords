35ccc77135a317a811fd9d9121b028f1da3c4cce
Jeffrey Hamlin <sourcesage@gmail.com>
2020-12-03 16:05:41 -0500

    Initialization Script

---
+++ SimpleWords/Database/Seed Data.sql
@@ -0,0 +1,75 @@
+use dev
+go
+SET IDENTITY_INSERT GameDev.LanguageType ON
+
+insert into GameDev.LanguageType
+(LANG_TYPE_ID, NAME)
+Values(1,'English')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1, 'AREA','a geographical region; tract:')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'ace', 'a playing card or die marked with or having the value indicated by a single spot:')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'act', 'anything done, being done, or to be done; deed; performance:')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'add', 'to unite or join so as to increase the number, quantity, size, or importance:')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
+
+insert into GameDev.WordList
+(LANG_TYPE_ID,WORD, DEFINITION)
+VALUES(1,'', '')
